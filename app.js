18efc101269ccb56bbcac22a40e780019a57e742
linklegend <link_leagend@hotmail.com>
2018-03-28 19:43:32 +1100

    Final version before submit

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
     console.log('Express serving at port ${PORT}');
 });
+
 app.set('view engine', 'ejs');
+app.use( express.static( "public" ) );
 app.use('/',route);
 app.use('/bye',route);
 app.use('/users/:id',route);
