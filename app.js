45131229a318a0a4bbab9df5d4f82c754a0bff92
Fahim Zaman <Fahim@Fahims-MacBook-Pro.local>
2018-03-29 22:34:19 +1100

    added a static thing

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const route = require("./routes/router.js");
 const PORT = process.env.PORT || 3000;
 
+
 app.listen(PORT, function(){
     console.log('Express serving at port ${PORT}');
 });
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use('/',route);
 app.use('/bye',route);
 app.use('/users/:id',route);
